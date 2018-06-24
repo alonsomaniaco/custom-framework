9dc84ed21773a3dc7dce5f82b7013f42494667e6
alonsomaniaco <abarrio@emergya.com>
2018-06-24 11:15:01 +0200

    Add template system

--- index.php
+++ index.php
@@ -1,30 +1,15 @@
 <?php
 
 use customFramework\helper\doctrine\DoctrineHelper;
+use customFramework\helper\twig\TwigHelper;
 use customFramework\model\Person;
 
 require_once 'vendor/autoload.php';
 
 $personRepository = DoctrineHelper::getRepository(Person::class);
+// Get an array of Person to use on foreach.
+$allPerson = $personRepository->findAll();
+TwigHelper::renderTemplate('index', [
+  'Persons' => $allPerson,
+]);
 
-?>
-
-<!doctype html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Listado de personas</title>
-</head>
-<body>
-<h1>Listado de personas</h1>
-<ul>
-  <?php
-  // Get an array of Person to use on foreach.
-  $allPerson = $personRepository->findAll();
-  foreach ($allPerson as $person) {
-    echo "<li><a href='persons.php?person=" . $person->getId() . "'>" . $person->getName() . "</a></li>";
-  }
-  ?>
-</ul>
-</body>
-</html>
--- persons.php
+++ persons.php
@@ -1,6 +1,7 @@
 <?php
 
 use customFramework\helper\doctrine\DoctrineHelper;
+use customFramework\helper\twig\TwigHelper;
 use customFramework\model\Person;
 
 require_once 'vendor/autoload.php';
@@ -8,22 +9,7 @@ require_once 'vendor/autoload.php';
 $idSelected = $_GET['person'];
 $personRepository = DoctrineHelper::getRepository(Person::class);
 $person = $personRepository->find($idSelected);
-?>
 
-<!doctype html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Datos de la persona</title>
-</head>
-<body>
-<a href="index.php">Volver</a>
-<h1>Datos de <?= $person->getName(); ?></h1>
-<ul>
-    <li>Lastname: <?= $person->getLastName(); ?></li>
-    <li>Age: <?= $person->getAge(); ?></li>
-    <li>Address: <?= $person->getAddress(); ?></li>
-    <li>Telephone: <?= $person->getTelephone(); ?></li>
-</ul>
-</body>
-</html>
+TwigHelper::renderTemplate('person', [
+  'Person' => $person,
+]);
